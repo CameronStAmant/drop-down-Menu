efb5dae68b25189b0fcbe5aac5c06979749b296b
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-04 13:42:50 -0700

    current testing

--- src/index.js
+++ src/index.js
@@ -10,6 +10,30 @@ const dropDownMenuFunction = function menu() {
         menu.classList.add(`${id.substring(0, id.length - 1)}`);
         change = true;
       }
+      const visibleMenus = document.getElementsByClassName('visible');
+      console.log(visibleMenus);
+      const visibleMenusArr = Array.from(visibleMenus);
+      visibleMenusArr.forEach((menu2) => {
+        if (menu2.id === 'menu2' && menu2.className === 'visible') {
+          menu2.classList.remove('visible');
+          const copyId = menu2.id;
+          const splitId = copyId.substring(0, copyId.length - 1);
+          menu2.classList.add(splitId);
+        } else {
+          console.log(`This is the menu2 item: ${menu2}`);
+          const close = menu2.closest('#menu2');
+          console.log(`This is close: ${close}`);
+          const copyId = document.getElementById(menu2);
+          console.log(`button id ${copyId}`);
+          menu2.classList.remove('visible');
+          console.log(copyid);
+          menu2.classList.add(copyId);
+          console.log('hi belower');
+        }
+      });
+      // menu.classList.remove('visible');
+      // menu.classList.add('menu');
+
       const menuItems = change
         ? document.getElementsByClassName('visible')
         : document.getElementsByClassName(`${id}`);
@@ -21,9 +45,9 @@ const dropDownMenuFunction = function menu() {
         if (change === false) {
           //minimize all open menus
           const closeOpenMenus = document.getElementsByClassName('visible');
-          console.log(closeOpenMenus);
+          // console.log(closeOpenMenus);
           const closeOpenMenusArr = Array.from(closeOpenMenus);
-          console.log(closeOpenMenusArr[0]);
+          // console.log(closeOpenMenusArr[0]);
           // closeOpenMenusArr.forEach((closeItem) => {
           //   if (closeItem.id !== null) {
           //     closeItem.classList.remove('visible');

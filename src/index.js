88f005a38646fce1123edaadd5c92160e6cd3656
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-04 15:03:40 -0700

    finish build

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ const menus = document.getElementsByClassName('menu');
 const menusArr = Array.from(menus);
 menusArr.forEach((item) => {
   item.addEventListener('click', () => {
-    const childNodes = Array.from(item.childNodes);
-    childNodes.forEach((menuItems) => {
-      if (menuItems.className === 'menuItems') {
-        menuItems.classList.remove('menuItems');
-        menuItems.classList.add('visible');
-      } else if (menuItems.className === 'visible') {
-        menuItems.classList.remove('visible');
-        menuItems.classList.add('menuItems');
-      }
-    });
+    const nextNode = item.nextSibling;
+    const nextnextNode = nextNode.nextSibling;
+    console.log(nextnextNode);
+    if (nextnextNode.className === 'menuItems') {
+      nextnextNode.classList.remove('menuItems');
+      nextnextNode.classList.add('visible');
+    } else if (nextnextNode.className === 'visible') {
+      nextnextNode.classList.remove('visible');
+      nextnextNode.classList.add('menuItems');
+    }
   });
 });
